e22b44d55a1325bbd380a211967be13e0363d956
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-10-03 13:14:47 +0800

    use vectored reads and writes

--- src/cppevent_net/util.hpp
+++ src/cppevent_net/util.hpp
@@ -6,7 +6,11 @@
 #include <cppevent_base/byte_buffer.hpp>
 
 #include <unistd.h>
+#include <sys/uio.h>
+
 #include <cerrno>
+
+#include <array>
 
 struct addrinfo;
 
@@ -19,16 +23,32 @@ inline bool can_read_buffer(io_chunk& chunk, byte_buffer<BUFFER_SIZE>& buffer) {
 }
 
 template<long BUFFER_SIZE>
+inline bool can_read_buffer(io_chunk_group& group, byte_buffer<BUFFER_SIZE>& buffer) {
+    group = buffer.get_read_chunks();
+    return group.m_count > 0;
+}
+
+template<long BUFFER_SIZE>
 inline bool can_write_buffer(io_chunk& chunk, byte_buffer<BUFFER_SIZE>& buffer) {
     chunk = buffer.get_write_chunk();
     return chunk.m_size > 0;
 }
 
 template<long BUFFER_SIZE>
+inline bool can_write_buffer(io_chunk_group& group, byte_buffer<BUFFER_SIZE>& buffer) {
+    group = buffer.get_write_chunks();
+    return group.m_count > 0;
+}
+
+template<long BUFFER_SIZE>
 inline OP_STATUS read_file(int fd, byte_buffer<BUFFER_SIZE>& buffer) {
-    io_chunk chunk;
-    while (can_write_buffer(chunk, buffer)) {
-        auto size_read = ::read(fd, chunk.m_ptr, chunk.m_size);
+    io_chunk_group group;
+    while (can_write_buffer(group, buffer)) {
+        std::array<iovec, 2> arr = {
+            iovec { group.m_chunks[0].m_ptr, static_cast<size_t>(group.m_chunks[0].m_size) },
+            iovec { group.m_chunks[1].m_ptr, static_cast<size_t>(group.m_chunks[1].m_size) }
+        };
+        auto size_read = ::readv(fd, arr.data(), group.m_count);
         if (size_read > 0) {
             buffer.increment_write_p(size_read);
         } else if (size_read == 0) {
@@ -44,9 +64,13 @@ inline OP_STATUS read_file(int fd, byte_buffer<BUFFER_SIZE>& buffer) {
 
 template<long BUFFER_SIZE>
 inline OP_STATUS write_file(int fd, byte_buffer<BUFFER_SIZE>& buffer) {
-    io_chunk chunk;
-    while (can_read_buffer(chunk, buffer)) {
-        auto size_written = ::write(fd, chunk.m_ptr, chunk.m_size);
+    io_chunk_group group;
+    while (can_read_buffer(group, buffer)) {
+        std::array<iovec, 2> arr = {
+            iovec { group.m_chunks[0].m_ptr, static_cast<size_t>(group.m_chunks[0].m_size) },
+            iovec { group.m_chunks[1].m_ptr, static_cast<size_t>(group.m_chunks[1].m_size) }
+        };
+        auto size_written = ::writev(fd, arr.data(), group.m_count);
         if (size_written > 0) {
             buffer.increment_read_p(size_written);
         } else if (errno == EAGAIN || errno == EWOULDBLOCK) {
